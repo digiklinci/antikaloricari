4ff08ccbcf80e84bcb016d8598317cab832241f9
digiklinci <165515192+digiklinci@users.noreply.github.com>
2024-05-12 23:18:29 +0200

    Ажурирање main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,32 +2,38 @@
 radio.onReceivedNumber(function (receivedNumber) {
     kalorijski_kusur = receivedNumber
     potrosene_kalorije = kalorijski_kusur
-    steps = kalorijski_kusur * 20
+    broj_koraka = kalorijski_kusur * broj_koraka_po_koloriji
     basic.showNumber(kalorijski_kusur)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    reset_dioda()
+    for (let index = 0; index < broj_koraka / broj_koraka_po_koloriji; index++) {
+        upali_diodu()
+    }
 })
-// resetovanje utrosenih kalorija i broja koraka
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(0)
-    steps = 0
+    broj_koraka = 0
     reset_dioda()
-    basic.clearScreen()
 })
 // kolicina kalorija koje su utrosene
 input.onGesture(Gesture.LogoDown, function () {
-    basic.showNumber(steps / 20)
+    basic.showNumber(broj_koraka / broj_koraka_po_koloriji)
     basic.clearScreen()
     reset_dioda()
 })
 input.onGesture(Gesture.Shake, function () {
-    steps += 1
-    if (steps % broj_koraka_dioda == 0) {
+    broj_koraka += 1
+    if (broj_koraka % broj_koraka_po_koloriji == 0) {
         upali_diodu()
     }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(steps)
+    basic.clearScreen()
+    basic.showNumber(broj_koraka)
 })
 function reset_dioda () {
+    basic.clearScreen()
     ledX = -1
     ledY = 0
 }
@@ -45,18 +51,17 @@ function upali_diodu () {
 }
 // slanje potrosenih kalorija
 input.onButtonPressed(Button.AB, function () {
-    // 20 koraka = 1 kal
-    potrosene_kalorije = steps / 20
+    potrosene_kalorije = broj_koraka / broj_koraka_po_koloriji
     radio.sendNumber(potrosene_kalorije)
-    basic.showIcon(IconNames.Heart)
 })
 let ledY = 0
 let ledX = 0
 let potrosene_kalorije = 0
 let kalorijski_kusur = 0
-let broj_koraka_dioda = 0
-let steps = 0
+let broj_koraka_po_koloriji = 0
+let broj_koraka = 0
 radio.setGroup(0)
-steps = 0
-broj_koraka_dioda = 10
+broj_koraka = 0
+// 20 koraka = 1Kcal
+broj_koraka_po_koloriji = 20
 reset_dioda()
